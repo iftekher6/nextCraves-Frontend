9c814f2e151bec446bcb95ecc7d0ad1ae8860cf2
Iftekher Ahmed <greatiftekher@gmail.com>
2025-06-30 08:09:05 +0600

    Dockerized App

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
 import Footer from "./components/Footer";
 import Header from "./components/Header";
 import Hero from "./components/Hero";
-import Search from "./components/Search";
+
 
 export default function Home() {
   return (
